c9303b302a200ddcbe41136676127004b137c609
JoseJuniorJJ <jose.junior@me.com.br>
2023-03-02 12:01:57 -0300

    Enums na prática

--- Pratica-Curso-Balta-Csharp/Pratica-Curso-Balta-Csharp/Program.cs
+++ Pratica-Curso-Balta-Csharp/Pratica-Curso-Balta-Csharp/Program.cs
@@ -1,27 +1,42 @@
 ﻿
 //O tipo da propriedade pode ser 'var' 
 //var mouse = new Product()
-Product mouse = new Product(1, "mouse", 99.99);
+Product mouse = new Product(1, "mouse", 99.99, EProductType.Product);
+var manutecaoEletrica = new Product(2, "Manutenção Elétrica Residencial", 200.00, EProductType.Service);
 
 Console.WriteLine(mouse.Id);
 Console.WriteLine(mouse.Name);
 Console.WriteLine(mouse.Price);
+Console.WriteLine(mouse.Type);
+Console.WriteLine((int)mouse.Type); //CONVERSOR EXPLÍCITO
 
+Console.WriteLine(manutecaoEletrica.Id);
+Console.WriteLine(manutecaoEletrica.Name);
+Console.WriteLine(manutecaoEletrica.Price);
+Console.WriteLine(manutecaoEletrica.Type);
 struct Product
 {
-    public Product(int id, string name, double price)
+    public Product(int id, string name, double price, EProductType type)
     {
         Id = id;
         Name = name;
         Price = price;
+        Type = type;
     }
     
     public int Id;
     public string Name;
     public double Price;
+    public EProductType Type;
 
     public double PriceInDolar(double dolar)
     {
         return Price * dolar;
     }
 }
+
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
+    }
