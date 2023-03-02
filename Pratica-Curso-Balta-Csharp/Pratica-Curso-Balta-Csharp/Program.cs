b504d2de3a1e63672770a3edee6dd0afe435c184
JoseJuniorJJ <jose.junior@me.com.br>
2023-03-02 11:33:14 -0300

    Structs na prática

---
+++ Pratica-Curso-Balta-Csharp/Pratica-Curso-Balta-Csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//O tipo da propriedade pode ser 'var' 
+//var mouse = new Product()
+Product mouse = new Product(1, "mouse", 99.99);
+
+Console.WriteLine(mouse.Id);
+Console.WriteLine(mouse.Name);
+Console.WriteLine(mouse.Price);
+
+struct Product
+{
+    public Product(int id, string name, double price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+    
+    public int Id;
+    public string Name;
+    public double Price;
+
+    public double PriceInDolar(double dolar)
+    {
+        return Price * dolar;
+    }
+}
